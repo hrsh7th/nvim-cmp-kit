9d7926993854aed0f3cbf1bfe9e3b249314b54d7
hrsh7th <629908+hrsh7th@users.noreply.github.com>
2025-05-08 11:28:47 +0900

    feat: add calc source

---
+++ lua/cmp-kit/ext/source/calc.lua
@@ -0,0 +1,75 @@
+local Async = require('cmp-kit.kit.Async')
+local TriggerContext = require('cmp-kit.core.TriggerContext')
+
+local PATTERN = [=[\s*\zs\%( \|math\.\w\+\|\d\+\%(\.\d\+\)\?\|[()*/+-]\)\+]=]
+
+local INVALID = {
+  isIncomplete = false,
+  items = {},
+}
+
+---@class cmp-kit.ext.source.calc.Option
+return function()
+  ---@type cmp-kit.core.CompletionSource
+  return {
+    name = 'calc',
+    get_configuration = function()
+      return {
+        keyword_pattern = PATTERN,
+        trigger_characters = { ')', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.' },
+      }
+    end,
+    complete = function()
+      return Async.run(function()
+        local ctx = TriggerContext.create()
+        local off = ctx:get_keyword_offset(PATTERN)
+        if not off then
+          return INVALID
+        end
+        local candidate_text = ctx.text_before:sub(off)
+
+        local stack = {}
+        for i = #candidate_text, 1, -1 do
+          local char = candidate_text:sub(i, i)
+          if char == ')' then
+            table.insert(stack, {
+              idx = i,
+              char = char,
+            })
+          elseif char == '(' then
+            if #stack == 0 then
+              return INVALID
+            end
+            table.remove(stack)
+          end
+        end
+
+        local program = candidate_text
+        if #stack > 0 then
+          program = candidate_text:sub(stack[#stack].idx)
+        end
+
+        local output = assert(loadstring(('return %s'):format(program), 'calc'))()
+        if type(output) ~= 'number' then
+          return INVALID
+        end
+
+        return {
+          isIncomplete = true,
+          items = {
+            {
+              label = program,
+              labelDetails = {
+                description = ('= %s'):format(output),
+              },
+              insertText = tostring(output),
+              filterText = candidate_text,
+            }
+          }
+        }
+      end):catch(function()
+        return INVALID
+      end)
+    end
+  }
+end
